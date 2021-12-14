1c1256d57f853de02a20ad9f64d939b5cbd0bf31
Spencer Heffley <spheffley@yahoo.com>
2021-12-13 22:49:21 -0500

    Added text box component, started making the new post section on home page

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
